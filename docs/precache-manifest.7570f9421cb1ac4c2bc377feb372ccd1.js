6f32b35e030d2e133356da9187e95b32a1c09df1
Thomas Junghans <thomas.junghans@gmail.com>
2020-05-24 23:17:52 +0200

    feat: build to docs

---
+++ docs/precache-manifest.7570f9421cb1ac4c2bc377feb372ccd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491afb6d23da6db2b917fce6f2429969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d7a5cff7a18b3a5e98a",
+    "url": "/static/css/main.5a96fa2e.chunk.css"
+  },
+  {
+    "revision": "a0a0d3b734520a149702",
+    "url": "/static/js/2.50079a5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50079a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7a5cff7a18b3a5e98a",
+    "url": "/static/js/main.7af8916c.chunk.js"
+  },
+  {
+    "revision": "7a7951182a1c76da4c71",
+    "url": "/static/js/runtime-main.f39fb773.js"
+  }
+]);
